cb9d55bf75cd4ea29c732fd6939b6e8a4242164c
amartyasalpekar <amartyasalpekar@gmail.com>
2024-08-30 22:40:43 -0700

    James Rodriguez

--- ShinyVanilla.R
+++ ShinyVanilla.R
@@ -60,9 +60,33 @@ for (period in 1:2) {
 
 ## James Rodirguez passes and assists. 
 
-james_pass <- StatsBombData%>%filter(type.name == "Pass" & is.na(pass.outcome.name), player.id == 5695)%>%
-  filter(pass.end_location.x>=102 & pass.end_location.y<=62 & pass.end_location.y>=18)
+james_pass <- StatsBombData%>%filter(type.name == "Pass" & is.na(pass.outcome.name), player.id == 5695)
 
+##Final Third Passes 
+
+passes_final_third <- StatsBombData%>%filter(type.name == "Pass", pass.end_location.x > 80)%>%
+  filter(!pass.outcome.name %in% c("Injury Clearance", "Unknown"))
+
+
+passes_final_third <- passes_final_third %>%
+  mutate(pass_success = ifelse(is.na(pass.outcome.name), "Successful", "Unsuccessful"))
+
+passes_final_third_count <- passes_final_third %>%
+  group_by(player.name) %>%
+  summarise(
+    total_passes = n(),
+    successful_passes = sum(pass_success == "Successful"),
+    unsuccessful_passes = sum(pass_success == "Unsuccessful")
+  ) %>%
+  ungroup()
+
+passes_final_third_count <- passes_final_third_count%>%filter(total_passes > 83)
+
+
+passes_final_third_count <- passes_final_third_count %>%
+  mutate(is_james = ifelse(player.name == "James David Rodríguez Rubio", TRUE, FALSE))
+
+## Copa America Final Argentina 
 
 ui <- navbarPage(
   "Copa America 2023-24 Analysis",
@@ -79,12 +103,15 @@ ui <- navbarPage(
            )
   ),
   
-  tabPanel("James Rodriguez Passes",
+  tabPanel("Copa America 2023-24 Top Players Analysis",
            mainPanel(
-             plotOutput("jamesPassesPlot")
+             plotOutput("jamesPassesPlot"),
+             plotOutput("finalThirdPassesPlot")
            )
   )
 )
+
+
 
 server <- function(input, output, session) {
   # Reactive expression to filter data based on selected teams
@@ -96,12 +123,10 @@ server <- function(input, output, session) {
     data %>% mutate(team.name = fct_reorder(team.name, shots))
   })
   
-  # Update selectInput choices based on available teams
   observe({
     updateSelectInput(session, "team", choices = unique(shots_goal$team.name))
   })
   
-  # Render the interactive plot using plotly
   output$shotsGoalsPlot <- renderPlotly({
     p <- ggplot(filteredData(), aes(x = team.name)) +
       geom_bar(aes(y = shots, text = paste("Shots:", shots)), stat = "identity", fill = "blue", alpha = 0.7) +
@@ -115,20 +140,47 @@ server <- function(input, output, session) {
   
   # Render the James Rodriguez plot
   output$jamesPassesPlot <- renderPlot({
-    james_pass <- StatsBombData %>%
-      filter(type.name == "Pass" & is.na(pass.outcome.name) & player.id == 5695) %>%
-      filter(pass.end_location.x >= 102 & pass.end_location.y <= 62 & pass.end_location.y >= 18)
     
     create_Pitch() + 
-      geom_segment(data = james_pass, aes(x = location.x, y = location.y, 
-                                          xend = pass.end_location.x, yend = pass.end_location.y),
-                   lineend = "round", size = 0.5, colour = "#000000", 
-                   arrow = arrow(length = unit(0.07, "inches"), ends = "last", type = "open")) + 
-      labs(title = "James Rodriguez, Completed Box Passes", 
+      geom_segment(data = james_pass, aes(x = location.x, y = location.y,
+                                          xend = pass.end_location.x, yend = pass.end_location.y,
+                                          colour = pass.goal_assist),
+                   lineend = "round", size = 0.5, 
+                   arrow = arrow(length = unit(0.07, "inches"), ends = "last", type = "open")) +
+      scale_colour_manual(values = c("FALSE" = "#000000", "TRUE" = "#FF0000"), 
+                          labels = c("FALSE" = "Other Passes", "TRUE" = "Assists"), # More descriptive legend labels
+                          name = "Pass Type") + # Updated legend title
+      labs(title = "Golden Ball Winner - James Rodriguez, All Passes with Assists Highlighted", 
            subtitle = "Copa America 2023-24") + 
       scale_y_reverse() + 
-      coord_fixed(ratio = 105/100)
+      coord_fixed(ratio = 105/100) + 
+      theme_minimal() + 
+      theme(
+        legend.position = "right",
+        legend.title = element_text(size = 10),
+        legend.text = element_text(size = 8),
+        plot.title = element_text(size = 14, face = "bold"),
+        plot.subtitle = element_text(size = 10)    
+      )
   })
+  
+  output$finalThirdPassesPlot <- renderPlot({
+    ggplot(passes_final_third_count, aes(x = reorder(player.name, -total_passes))) +
+      geom_bar(aes(y = successful_passes, fill = "Successful Passes"), stat = "identity", position = "dodge", color = "black") +
+      geom_bar(aes(y = unsuccessful_passes, fill = "Unsuccessful Passes"), stat = "identity", position = "dodge", color = "black") +
+      scale_fill_manual(values = c("Successful Passes" = "green", "Unsuccessful Passes" = "red")) +
+      labs(title = "James Rodríguez vs. Top 10 Final Third Passers",
+           x = "Player",
+           y = "Number of Passes") +
+      theme_minimal() +
+      theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
+      geom_bar(data = passes_final_third_count %>% filter(player.name == "James David Rodríguez Rubio"),
+               aes(y = successful_passes, fill = "Successful Passes"), color = "black", stat = "identity", position = "dodge", fill = "green") +
+      geom_bar(data = passes_final_third_count %>% filter(player.name == "James David Rodríguez Rubio"),
+               aes(y = unsuccessful_passes, fill = "Unsuccessful Passes"), color = "black", stat = "identity", position = "dodge", fill = "red") +
+      labs(fill = "Pass Type")
+  })
+  
 }
 
 # Run the application 
